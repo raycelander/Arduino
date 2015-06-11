5bc2ca3b40f41c5f035de69c936010ecdc741eb8
Rainer Hirter <r.hirter@gmail.com>
2015-06-11 12:29:17 +0200

    Power saving

--- Wasserpumpe/Wasserpumpe.ino
+++ Wasserpumpe/Wasserpumpe.ino
@@ -1,32 +1,28 @@
-const int ledFeuchtigkeit = 10;
 const int ledMessen = 9;
-const int devSensor = 13;
-const int devPumpe = 12;
-int val = 0;
+const int devSensorPower = 10;
+const int devPumpeOutput = 12;
+const int devSensorInput = 13;
 
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(devSensor, INPUT);
-  pinMode(devPumpe, OUTPUT);
-  pinMode(ledFeuchtigkeit, OUTPUT);
+  pinMode(devSensorInput, INPUT);
+  pinMode(devPumpeOutput, OUTPUT);
+  pinMode(devSensorPower, OUTPUT);
   pinMode(ledMessen, OUTPUT);
-  //Serial.begin(9600);
-  //Serial.println("Starting");
 }
 
 void loop() {
-  val = digitalRead(devSensor);
-  digitalWrite(ledFeuchtigkeit, val == 1 ? LOW : HIGH);
-  //Serial.println("val: " + val);
-  while(val == 1){
-    digitalWrite(devPumpe, HIGH);
+  digitalWrite(ledMessen, HIGH); 
+  digitalWrite(devSensorPower, HIGH); // Sensor starten
+  delay(1000);
+
+  while(digitalRead(devSensorInput) == 1){
+    digitalWrite(devPumpeOutput, HIGH);
     delay(4000);  
-    digitalWrite(devPumpe, LOW);
-    digitalWrite(ledMessen, HIGH);
+    digitalWrite(devPumpeOutput, LOW);
     delay(10000);
-    val = digitalRead(devSensor);
-    digitalWrite(ledMessen, LOW);
   }
-  digitalWrite(ledFeuchtigkeit, val == 1 ? LOW : HIGH);
+
+  digitalWrite(ledMessen, LOW);
+  digitalWrite(devSensorPower, LOW); // Sensor ausschalten
   delay(900000); 
 }
