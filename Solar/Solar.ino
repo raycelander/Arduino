585ed74251d2ffbceb71ca504d3bf363bd63c226
Rainer Hirter <r.hirter@gmail.com>
2015-06-16 20:10:52 +0200

    LCD attached to watch battery status

--- Solar/Solar.ino
+++ Solar/Solar.ino
@@ -1,54 +1,39 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int batteryValue = 0;
 float outputValue = 0;
-float oldValue = 0;
-int timer = 0;
+float maxValue = 0;
+float minValue = 10;
+
 void setup() {
   // initialize digital pin 13 as an output.
-  Serial.println("Initializing");
-  Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+    Serial.println("Initializing");
+    Serial.begin(9600);
+   lcd.begin(20,4);
+   lcd.setCursor(3, 1);
+   lcd.print("Solarpanel 0.1");
 }
 
 
 void loop() {
-  timer = timer + 1;
+  delay(1000);
+  lcd.clear();
   batteryValue = analogRead(A0);
   outputValue = (float(batteryValue)*5)/1023*2;
-  Serial.println(timer);  
-  int v = outputValue;
-  if (timer == 40){
-      if (oldValue > 0){
-        for (int i=0;i<3;i++){
-          digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-          digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-          delay(100);
-          digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
-          digitalWrite(11, LOW);   // turn the LED on (HIGH is the voltage level)
-          delay(100);
-        }
-        
-        if (oldValue < outputValue){
-          digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-        }else if(oldValue > outputValue){
-          digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-        }else{
-          digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-          digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-        }
-        Serial.println("outputValue: " + String(outputValue) + " / oldValue: " + String(oldValue)); 
-   }
-  oldValue = outputValue;
-  timer = 0;
- }
-  for (int i=0;i<v;i++){
-    digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(100);              // wait for a second
-    digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-    delay(100);
+  float v = outputValue;
+  if (v < minValue){
+    minValue = v;
   }
- 
-  delay(3000);
+  if (maxValue < v){
+    maxValue = v;
+  }
+  lcd.setCursor(0,0);
+  lcd.print("V cur: " + String(v)); 
+  lcd.setCursor(0,1);
+  lcd.print("V max: " + String(maxValue));
+  lcd.setCursor(0,2);
+  lcd.print("V min: " + String(minValue));
 }
