94a487f22a9621c71822da126b67af0d7d833648
Rainer Hirter <r.hirter@gmail.com>
2015-06-17 12:29:48 +0200

    Refresh values only

--- Solar/Solar.ino
+++ Solar/Solar.ino
@@ -4,36 +4,39 @@
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 int batteryValue = 0;
-float outputValue = 0;
-float maxValue = 0;
-float minValue = 10;
+float v = 0;
+float maxV = 0;
+float minV = 10;
 
 void setup() {
-  // initialize digital pin 13 as an output.
-    Serial.println("Initializing");
     Serial.begin(9600);
-   lcd.begin(20,4);
-   lcd.setCursor(3, 1);
-   lcd.print("Solarpanel 0.1");
+    lcd.begin(20,4);
+    lcd.setCursor(3, 1);
+    lcd.print("Solarpanel 0.1");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("V cur:"); 
+    lcd.setCursor(0,1);
+    lcd.print("V max:");
+    lcd.setCursor(0,2);
+    lcd.print("V min:");
 }
 
-
 void loop() {
-  delay(1000);
-  lcd.clear();
-  batteryValue = analogRead(A0);
-  outputValue = (float(batteryValue)*5)/1023*2;
-  float v = outputValue;
-  if (v < minValue){
-    minValue = v;
-  }
-  if (maxValue < v){
-    maxValue = v;
-  }
-  lcd.setCursor(0,0);
-  lcd.print("V cur: " + String(v)); 
-  lcd.setCursor(0,1);
-  lcd.print("V max: " + String(maxValue));
-  lcd.setCursor(0,2);
-  lcd.print("V min: " + String(minValue));
+    batteryValue = analogRead(A0);
+    v = (float(batteryValue)*5)/1023*2;
+    if (v < minV){
+      minV = v;
+    }
+    if (maxV < v){
+      maxV = v;
+    }
+    lcd.setCursor(7,0);
+    lcd.print(String(v)); 
+    lcd.setCursor(7,1);
+    lcd.print(String(maxV));
+    lcd.setCursor(7,2);
+    lcd.print(String(minV));
+    delay(1000);
 }
