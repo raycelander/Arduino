247a509659d5c281b2892d14a15814a517fada6a
Rainer Hirter <r.hirter@gmail.com>
2015-06-24 12:46:46 +0200

    New wiring

--- Wasserpumpe/Wasserpumpe.ino
+++ Wasserpumpe/Wasserpumpe.ino
@@ -1,29 +1,24 @@
-const int ledMessen = 9;
-const int devSensorPower = 10;
+const int devPumpeOutput = 12;
+const int devSensorLedOutput = 13;
 const int devSensorInput = 0;
-const int devPumpePower = 13;
-
 
 void setup() {
   pinMode(devSensorInput, INPUT);
-  pinMode(devPumpePower, OUTPUT);
-  pinMode(devSensorPower, OUTPUT);
-  pinMode(ledMessen, OUTPUT);
+  pinMode(devPumpeOutput, OUTPUT);
+  pinMode(devSensorLedOutput, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(ledMessen, HIGH); 
-  digitalWrite(devSensorPower, HIGH); // Sensor starten
+  digitalWrite(devSensorLedOutput, HIGH); // Sensor starten
   delay(1000);
 
   while(analogRead(devSensorInput) < 310){
-    digitalWrite(devPumpePower, HIGH);
+    digitalWrite(devPumpeOutput, HIGH);
     delay(4000);  
-    digitalWrite(devPumpePower, LOW);
+    digitalWrite(devPumpeOutput, LOW);
     delay(10000);
   }
 
-  digitalWrite(ledMessen, LOW);
-  digitalWrite(devSensorPower, LOW); // Sensor ausschalten
+  digitalWrite(devSensorLedOutput, LOW); // Sensor ausschalten
   delay(900000); 
 }
